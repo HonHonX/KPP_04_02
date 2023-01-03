e6eb0554a838c8acb5af11432f824e532b11fd65
Sherry <119943082+HonHonX@users.noreply.github.com>
2023-01-03 15:08:20 +0100

    Update vergleich.c

--- vergleich.c
+++ vergleich.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "vergleich.h"
+#include "groessereZahl.h"
 
 //Hauptprogramm
 int main () {
@@ -27,7 +27,7 @@ int main () {
   scanf("%d", &zahl2);
   
   //Ausgabe
-  printf("\nDie größere Zahl der beiden Zahlen lautet %d.\n",vergleich(zahl1,zahl2)); 
+  printf("\nDie größere Zahl der beiden Zahlen lautet %d.\n",groessereZahl(zahl1,zahl2)); 
     
   //Ende
   printf("\n\n");
